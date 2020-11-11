06ce1197b1482f470d5d30efe7463bad2261e2a0
joao <joaovtolente@gmail.com>
2020-11-11 18:39:13 -0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
